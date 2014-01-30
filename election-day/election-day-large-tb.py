75a4546f62b3912f5a17a3f6690cf1f6dbdcffe3
Torgeir Børresen <torgebo@ulrik.uio.no>
2014-01-30 03:38:30 +0100

    Analytical solution for election-day large input

---
+++ election-day/election-day-large-tb.py
@@ -0,0 +1,26 @@
+
+#N log(N) solution (simulation)
+import math;
+
+elim = [False];
+
+
+class Solver:
+
+	def __init__(self):
+		self.elim = [False];
+
+	def solve_const(self,N):
+		p = int(math.floor(math.log(N, 2)));
+		l = N % 2**p;
+		return 2*l+1;
+
+it = 20000;
+N = 1000000000000000000000000000000;
+i=0;
+sol = Solver();
+while(i<it):
+	i+=1;
+	s=sol.solve_const(N);
+print s;
+
