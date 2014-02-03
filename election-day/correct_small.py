fceeb2b6b7b354539775c3dd9f0c6457a66a1ee4
Martin Vonheim Larsen <martin@melvon.com>
2014-02-03 19:34:56 +0100

    Added solution for small testcases
    This version passes tests on the judge

---
+++ election-day/correct_small.py
@@ -0,0 +1,42 @@
+from sys import stdin
+
+class Solver:
+    def __init__(self):
+        self.elim = [False];
+
+    def get_next_next(self, i):
+        elim = self.elim;
+        N = len(elim);
+        j=i;
+        one = False;
+
+        while(True):
+            j=j+1; j=j% N;
+
+            if(not one and j==i):
+                return -1;
+            elif(not one and not elim[j]):
+                one = True;
+            elif(one and not elim[j]):
+                return j;
+
+    def solve_iter(self,N):
+        self.elim = N*[False];
+        elim = self.elim;
+
+        if(N==1):
+            return 1;
+
+        i = 1;
+        while(True):
+            j = self.get_next_next(i);
+
+            if(j==-1):
+                return i+1;
+
+            elim[i] = True;
+            i = j;
+
+for line in stdin:
+    sol = Solver();
+    print sol.solve_iter(int(line))
