9c93e8407d55d6d45b98d36d239fa3135598b01d
Martin Vonheim Larsen <martin@melvon.com>
2014-02-10 12:44:06 +0100

    Added solution and testcases

---
+++ rps/correct_martinvl.py
@@ -0,0 +1,16 @@
+from sys import stdin
+
+T = int(stdin.readline())
+N = int(1e4)
+
+choice = {'Rock':'Paper', 'Paper':'Scissors', 'Scissors':'Rock'}
+
+for i in xrange(T):
+    for j in xrange(N):
+        you, friend = stdin.readline().split()
+        second = choice[friend]
+
+        if second == you:
+            second = friend
+
+        print second
