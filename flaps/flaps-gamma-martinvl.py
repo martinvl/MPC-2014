ae63c568417d861889cd23221f84950e7d214902
Martin Vonheim Larsen <martin@melvon.com>
2014-02-23 00:05:16 +0100

    Small modification to match WolframAlpha definition

--- flaps/flaps-gamma-martinvl.py
+++ flaps/flaps-gamma-martinvl.py
@@ -1,11 +1,11 @@
 # @EXPECTED_RESULTS@: CORRECT
 
 from sys import stdin
-from scipy.special import gamma, gammainc
+from scipy.special import gamma, gammaincc
 from scipy import exp, log
 
 def v_integral(v0, t, p):
-    return v0 * exp(p*(1 - log(p)))*gammainc(p + 1, p + t)*gamma(p + 1)
+    return -v0*exp(p)*pow(p, -p)*gammaincc(p + 1, p + t)*gamma(p + 1)
 
 def pos(v0, t, p):
     return v_integral(v0, t, p) - v_integral(v0, 0, p)
