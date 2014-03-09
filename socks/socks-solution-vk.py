0eadc162d1954e9421c342de6c0935f00337e449
Martin Vonheim Larsen <martin@melvon.com>
2014-03-09 17:48:59 +0100

    Socks: Fixed minor bug in solution

--- socks/socks-solution-vk.py
+++ socks/socks-solution-vk.py
@@ -6,14 +6,14 @@ N = int(stdin.readline())
 frequency = {}
 for test_case in range(N):
     label = stdin.readline().strip()
-    if label in frequency.keys():
+    if label in frequency:
         frequency[label] += 1
     else:
         frequency[label] = 1
 
 socksess = True
 remove_list = []
-for label in frequency.keys():
+for label in frequency:
     if frequency[label] == 1:
         remove_list.append(label)
         socksess = False
