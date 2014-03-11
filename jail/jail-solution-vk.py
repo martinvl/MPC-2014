ee81d59ce61772c03ac5e39420f790f9d6363a9d
Torgeir Børresen <torgebo@ulrik.uio.no>
2014-03-11 19:16:29 +0100

    Changed variable names

--- jail/jail-solution-vk.py
+++ jail/jail-solution-vk.py
@@ -6,39 +6,39 @@ from sys import stdin
 
 # Input
 l = stdin.readline().split()
-X = int(l[0])
-Y = int(l[1])
-M = int(l[2])
+M = int(l[0])
+N = int(l[1])
+I = int(l[2])
 
 # Initialize grid
-grid = [[0]*Y for i in range(X)]
+grid = [[0]*N for i in range(M)]
 # Input vertical walls
 vertical_walls = []
-for i in range(X):
+for i in range(M):
 	l = stdin.readline().split()
 	vertical_walls.append([int(k) for k in l])
 
 # Input horizontal walls
 horizontal_walls = []
-for i in range(X-1):
+for i in range(M-1):
 	l = stdin.readline().split()
 	horizontal_walls.append([int(k) for k in l])
 
 # Init first row and first col
-grid[0][0] = M
-for i in range(1, X):
+grid[0][0] = I
+for i in range(1, M):
 	grid[i][0] = grid[i-1][0]
 	if horizontal_walls[i-1][0] == 1:
 		grid[i][0] -= 1
 
-for i in range(1, Y):
+for i in range(1, N):
 	grid[0][i] = grid[0][i-1]
 	if vertical_walls[0][i-1] == 1:
 		grid[0][i] -= 1
 
 # Use DP on the remiander of the grid
-for i in range(1, X):
-	for j in range(1, Y):
+for i in range(1, M):
+	for j in range(1, N):
 		from_x = grid[i-1][j]
 		if horizontal_walls[i-1][j] == 1:
 			from_x -= 1
@@ -46,4 +46,4 @@ for i in range(1, X):
 		if vertical_walls[i][j-1] == 1:
 			from_y -= 1
 		grid[i][j] = max(from_x, from_y)
-print max(0, grid[X-1][Y-1])
+print max(0, grid[M-1][N-1])
