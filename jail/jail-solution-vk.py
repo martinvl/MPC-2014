fca2a434747790ef9aa1e46e13c2114d4848af67
Vidar Klungre <Vidar@1x-193-157-205-127.uio.no>
2014-03-12 11:48:46 +0100

    Jail: Solution did not work, had to switch M and N.

--- jail/jail-solution-vk.py
+++ jail/jail-solution-vk.py
@@ -11,34 +11,34 @@ N = int(l[1])
 I = int(l[2])
 
 # Initialize grid
-grid = [[0]*N for i in range(M)]
+grid = [[0]*M for i in range(N)]
 # Input vertical walls
 vertical_walls = []
-for i in range(M):
+for i in range(N):
 	l = stdin.readline().split()
 	vertical_walls.append([int(k) for k in l])
 
 # Input horizontal walls
 horizontal_walls = []
-for i in range(M-1):
+for i in range(N-1):
 	l = stdin.readline().split()
 	horizontal_walls.append([int(k) for k in l])
 
 # Init first row and first col
 grid[0][0] = I
-for i in range(1, M):
+for i in range(1, N):
 	grid[i][0] = grid[i-1][0]
 	if horizontal_walls[i-1][0] == 1:
 		grid[i][0] -= 1
 
-for i in range(1, N):
+for i in range(1, M):
 	grid[0][i] = grid[0][i-1]
 	if vertical_walls[0][i-1] == 1:
 		grid[0][i] -= 1
 
 # Use DP on the remiander of the grid
-for i in range(1, M):
-	for j in range(1, N):
+for i in range(1, N):
+	for j in range(1, M):
 		from_x = grid[i-1][j]
 		if horizontal_walls[i-1][j] == 1:
 			from_x -= 1
@@ -46,4 +46,4 @@ for i in range(1, M):
 		if vertical_walls[i][j-1] == 1:
 			from_y -= 1
 		grid[i][j] = max(from_x, from_y)
-print max(0, grid[M-1][N-1])
+print max(0, grid[N-1][M-1])
